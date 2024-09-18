fc478b02169c3ef2de9d46e7183b63206cd95def
Dao Thi Thu Hong <97780307+DaoHongDS@users.noreply.github.com>
2024-09-18 23:17:02 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Binance API keys => insert your own keys
-API_KEY = 'jooU4clrxiGLthm4zIqCYBt5ktEnat091Ces6cC8ElVoIhR8F9ZfvgxUgzvhjxAx'
-API_SECRET = 'h4TPDN20IYGzmN5wqG6yQLiN4uU7VWcFb7CpZB33nlpijpPiErxCz7lniQjKvWyx'
+API_KEY = ''
+API_SECRET = ''
 
 BTC_FILE = 'data/BTC_history.csv'
 SA_FILE = 'data/SA_by_time.csv'
